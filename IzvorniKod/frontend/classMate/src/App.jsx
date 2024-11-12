06b3eff66b6f886c56946e5d1c279e5381d41eb1
MislavLukaSulic <mislav-luka.sulic@fer.unizg.hr>
2024-11-12 15:53:12 +0100

    Napravljen Main page
    postavljena stanica sa koje će se birati sve ostale usluge kao što su materijali predmeta, raspored i poruke

--- IzvorniKod/frontend/classMate/src/App.jsx
+++ IzvorniKod/frontend/classMate/src/App.jsx
@@ -1,12 +1,13 @@
 import RegisterPage from './pages/RegisterPage'
 import LandingPage from './pages/LandingPage'
+import MainPage from './pages/MainPage'
 
 // Ako želite da se prikaže LandingPage, zamijenite RegisterPage s LandingPage u liniji 9.
 function App() {
 
   return (
     <div>
-        <LandingPage />
+        <MainPage />
     </div>
   )
 }
