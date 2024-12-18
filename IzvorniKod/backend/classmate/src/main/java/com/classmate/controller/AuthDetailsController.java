00dddb1e6e00c4f452fec7cb6ae87b28c25ea033
marinb2 <marin.brkic@fer.unizg.hr>
2024-12-18 18:22:17 +0100

    iznova napisana logika za RegisterPage i izmijenjen Controller za dohvat auth access tokena

--- IzvorniKod/backend/classmate/src/main/java/com/classmate/controller/AuthDetailsController.java
+++ IzvorniKod/backend/classmate/src/main/java/com/classmate/controller/AuthDetailsController.java
@@ -1,19 +1,21 @@
 package com.classmate.controller;
 
+import java.util.Collections;
 import java.util.Map;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("auth/details")
+@RequestMapping("auth")
 public class AuthDetailsController {
 
-    @GetMapping("/currentuser")
-    public Map<String, Object> userDetails(@AuthenticationPrincipal OAuth2User user) {
-        return user.getAttributes();
+    @GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("email", principal.getAttribute("email"));
     }
 }
--- IzvorniKod/backend/classmate/src/main/java/com/classmate/controller/UserController.java
+++ IzvorniKod/backend/classmate/src/main/java/com/classmate/controller/UserController.java
@@ -36,4 +36,5 @@ public class UserController {
         return userService.getUserById(id);
     }
 
+    
 }
