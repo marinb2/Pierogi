d99d6a0100a109d188baa3e2b681ca83cb720d15
marinb2 <marin.brkic@fer.unizg.hr>
2024-12-22 17:37:50 +0100

    cors configure

--- IzvorniKod/backend/classmate/src/main/java/com/classmate/controller/NotificationController.java
+++ IzvorniKod/backend/classmate/src/main/java/com/classmate/controller/NotificationController.java
@@ -8,6 +8,7 @@ import com.classmate.service.NotificationService;
 
 @RestController
 @RequestMapping("/api/notifications")
+@CrossOrigin("*")
 public class NotificationController {
 
     @Autowired
